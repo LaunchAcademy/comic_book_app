1b82817dcb577ca05ffe6b479471e019dfb966e3
Richard Davis <rdavis.bacs@gmail.com>
2014-09-22 15:33:36 -0400

    added delete user feature

---
+++ spec/features/user_updates_profile_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'user updates profile', %Q(
+
+  As an authenticated user
+  I want to update my information
+  So that I can keep my profile up to date
+
+) do
+
+  scenario 'user changes password' do
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    click_link 'Sign in'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+
+    click_link 'Edit profile'
+    fill_in 'Current password', with: user.password
+    fill_in 'Password', with: 'new_password'
+    fill_in 'Password confirmation', with: 'new_password'
+    click_button 'Update'
+
+    expect(page).to have_content("updated successfully")
+  end
+
+end
