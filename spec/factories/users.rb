0af5abacf3e0ec87ab94d452ea4ea771ea7697b2
Richard Davis <rdavis.bacs@gmail.com>
2014-09-22 14:40:34 -0400

    user signs up feature complete

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
+    sequence(:email) { |n| "generic_superhero_reference#{n}@gmail.com" }
+    password 'secret123'
+    password_confirmation 'secret123'
   end
 end
--- spec/features/user_creates_a_new_account_spec.rb
+++ spec/features/user_creates_a_new_account_spec.rb
@@ -28,4 +28,37 @@ feature 'user creates a new account', %Q(
 
     expect(page).to have_content("Welcome! You have signed up successfully.")
   end
+
+  scenario 'user forgets fields' do
+    visit root_path
+    click_link 'Sign up'
+    click_button 'Sign up'
+    expect(page).to have_content("can't be blank")
+  end
+
+  scenario 'email already exists' do
+    user = User.create(email: 'aquaman@gmail.com',
+      password: 'fisharecool123', password_confirmation: 'fisharecool123')
+
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email', with: 'aquaman@gmail.com'
+    fill_in 'Password', with: 'secret123'
+    fill_in 'Password confirmation', with: 'secret123'
+    click_button 'Sign up'
+
+    expect(page).to have_content('Email has already been taken')
+  end
+
+  scenario 'passwords do not match' do
+    visit root_path
+    click_link 'Sign up'
+
+    fill_in 'Email', with: 'captian_america_king@gmail.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '87654321'
+    click_button 'Sign up'
+
+    expect(page).to have_content("Password confirmation doesn't match")
+  end
 end
