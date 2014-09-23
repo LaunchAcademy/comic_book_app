921bee3d891d73040d6dd0c95e752ac59bb0c79e
RJ Robinson <robertj.robinson@gmail.com>
2014-09-23 17:38:44 -0400

    created failing test

---
+++ spec/features/user_adds_a_rating_to_comic_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'user tries to add a review to a comic', %Q(
+
+  As a CBN
+  I want to add a review to a comic book
+  So I can share my thoughts to the site.
+
+) do
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario 'user adds review to comic book' do
+      sign_in(user)
+      comic = FactoryGirl.build(:comic_book)
+
+      visit new_comic_book_path
+      fill_in 'Title', with: comic.title
+      fill_in 'Publisher', with: comic.publisher
+      fill_in 'Author', with: comic.author
+      fill_in 'Artist', with: comic.artist
+      fill_in 'Description', with: comic.description
+      click_button 'Create Comic Book'
+      click_link 'Add Review'
+  end
+end # of test
