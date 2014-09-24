c10bc0d459577257ada84ceb9e5511f504849249
Robert Petrowsky <bobpetrowsky@gmail.com>
2014-09-24 16:05:36 -0400

    added spec for pagination

---
+++ spec/features/pagination_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'user can see only 10 comics on a page', %Q(
+
+  As a CBN
+  I want to only see 10 comics per page
+  So I don't have to sort through too much Capt. Ameriwuss.
+
+) do
+
+  scenario 'user only can see 10 comics per page' do
+    20.times { FactoryGirl.create(:comic_book) }
+
+    visit comic_books_path
+
+    expect(page).to have_selector('.comic_book', count: 10)
+
+  end
+end
