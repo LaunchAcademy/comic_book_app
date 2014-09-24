4012545d3b5272b36e311e15ad4e12963c7e88e0
RJ Robinson <robertj.robinson@gmail.com>
2014-09-24 13:37:28 -0400

    adding edit function

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,6 +4,7 @@ class Rating < ActiveRecord::Base
   belongs_to :user
 
   validates :rating, presence: true
-
+  validates :user_id, presence: true
+  validates :comic_book_id, presence: true
 
 end # of class
