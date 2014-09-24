a7bb3ad2782e46433a510b6ef499f42a5a80fb6e
Robert Petrowsky <bobpetrowsky@gmail.com>
2014-09-24 16:39:34 -0400

    changed search query statement

--- app/models/comic_book.rb
+++ app/models/comic_book.rb
@@ -4,6 +4,6 @@ class ComicBook < ActiveRecord::Base
   validates :title, presence: true
 
   def self.search(query)
-    where("title like ?", "%#{query}%")
+    where("title ilike %?%", query)
   end
 end
