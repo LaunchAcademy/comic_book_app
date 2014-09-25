65a951a2232167383aed46cfb79abf8a4901391c
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-09-25 16:37:36 -0400

    The image upload test works.

--- spec/features/user_creates_comic_book_with_cover_spec.rb
+++ spec/features/user_creates_comic_book_with_cover_spec.rb
@@ -14,27 +14,18 @@ feature 'user can upload a comic book cover', %Q(
     sign_in(user)
     comic = FactoryGirl.build(:comic_book)
 
-
-
     visit new_comic_book_path
-
-
-
     fill_in 'Title', with: comic.title
     fill_in 'Publisher', with: comic.publisher
     fill_in 'Author', with: comic.author
     fill_in 'Artist', with: comic.artist
     fill_in 'Description', with: comic.description
-    attach_file 'comic_book_cover', comic.cover
-
-
+    attach_file 'comic_book_cover', "#{Rails.root}/app/assets/images/deadpool_cover.jpg"
     click_button 'Create Comic Book'
 
-    comic.reload
 
-
-
+    saved_comic_book = ComicBook.find_by(title: comic.title)
     expect(page).to have_content("Successfully created!")
-    expect(page).to have_css("img[src='#{comic.cover}']")
+    expect(page).to have_css("img[src='#{saved_comic_book.cover}']")
   end
 end
