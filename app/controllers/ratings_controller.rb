14277cd22e5a216d308690278fa9f76eb533d727
RJ Robinson <robertj.robinson@gmail.com>
2014-09-25 09:38:56 -0400

    removed edit feature for now - This will be a later project

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,9 +1,10 @@
 class RatingsController < ApplicationController
 
   def create
+    @user = current_user
     @comic_book = ComicBook.find(params[:comic_book_id])
     @rating = @comic_book.ratings.new(rating_params)
-    @rating.user = current_user
+    @rating.user_id = @user.id
 
 
     if @rating.save
@@ -12,10 +13,25 @@ class RatingsController < ApplicationController
     else
       redirect_to comic_book_path(@comic_book), notice: 'There was an error'
     end
+  end
+
+  def edit
+    @rating = Rating.find(params[:id])
+    @comic_book = ComicBook.find(params[:comic_book_id])
+  end
 
 
-  end # of class
-
+## FINISH THIS LATER ##
+  # def update
+  #   @rating = Rating.find(params[:id])
+  #
+  #   if @rating.update(rating_params)
+  #     redirect_to @comic_book, notice: 'Review Updated'
+  #   else
+  #     flash[:notice] = 'Could not update. Ask Batman. '
+  #     redirect_to @comic_book
+  #   end
+  # end
 
 
   #########
@@ -25,4 +41,9 @@ class RatingsController < ApplicationController
   def rating_params
     params.require(:rating).permit( :body, :rating )
   end
-end
+
+
+
+
+
+end # of class
