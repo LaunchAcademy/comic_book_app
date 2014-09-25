b19b37c1fd75eefb96d8557658518036187973a5
Robert Petrowsky <bobpetrowsky@gmail.com>
2014-09-25 09:41:02 -0400

    changed spec

--- spec/features/search_fails_spec.rb
+++ spec/features/search_fails_spec.rb
@@ -15,7 +15,7 @@ feature 'User searches for a comic that does not exist.', %Q(
 
     fill_in 'search', with: '393urjiodjf903uf9'
     click_button 'Search'
-    save_and_open_page
+
     expect(page).to have_content('Could not find thy query.')
 
   end
