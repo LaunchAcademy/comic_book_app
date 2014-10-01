f32c2b6879b4f3107141bf2ce06504ddd0c5669b
Robert Petrowsky <bobpetrowsky@gmail.com>
2014-09-30 21:44:57 -0400

    fixed spec for delete reviews

---
+++ spec/features/user_deletes_a_review_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'user tries to delete rating', %Q(
+
+  As a CBN
+  I want to delete a rating I created
+  So I can stop wasting my time on Capt. Ameriwuss.
+
+) do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:comic_book) { FactoryGirl.create(:comic_book) }
+
+  scenario 'user edits rating to comic book' do
+      sign_in(user)
+
+      visit comic_book_path(comic_book)
+      fill_in 'Body', with: 'sample review'
+      select(3, from: 'Rating')
+      click_button 'Create Rating'
+      click_link 'Delete Rating'
+
+
+      expect(page).to have_content('This rating has been removed.')
+  end
+end # of test
