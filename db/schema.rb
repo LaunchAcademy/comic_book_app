338cfb93f1acddfce1431acc81cfb8bcc46c745b
RJ Robinson <robertj.robinson@gmail.com>
2014-10-03 10:34:54 -0400

    added seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002140434) do
+ActiveRecord::Schema.define(version: 20141003140237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141002140434) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "cover"
+    t.string   "barcode"
   end
 
   create_table "reviews", force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+comic_books = []
+
+options = { headers: :true, col_sep: "\t" }
+
+CSV.foreach("#{Rails.root}/lib/assets/comics.csv", options) do |comic|
+  comic_books << comic.to_hash
+end
+
+
+comic_books.each do |comic_attr|
+  comic = ComicBook.find_or_initialize_by(barcode: comic_attr["Barcode"] )
+  comic.title = comic_attr["Series"]
+  comic.issue = comic_attr["Issue No."]
+  comic.publisher = comic_attr["Publisher"]
+  comic.author = comic_attr["Writer"]
+  comic.artist = comic_attr["Cover Artist"]
+  comic.description = comic_attr["Plot"]
+  comic.save!
+end
