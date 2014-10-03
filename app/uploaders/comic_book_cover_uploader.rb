b27eb036bf0473814cb6fcd4a3fa981a5826ddd2
RJ Robinson <robertj.robinson@gmail.com>
2014-10-03 15:56:22 -0400

    Added get_cover_art feature.

--- app/uploaders/comic_book_cover_uploader.rb
+++ app/uploaders/comic_book_cover_uploader.rb
@@ -15,4 +15,8 @@ class ComicBookCoverUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
+
+  def default_url
+    "http://image.shutterstock.com/display_pic_with_logo/425641/99491360/stock-photo-no-comic-book-explosion-99491360.jpg"
+  end
 end
